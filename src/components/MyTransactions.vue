885d7813bb4872b3da3d5e5664dcf6a42299b95a
Karan Jain <karan.jain@sap.com>
2023-07-02 13:14:46 +0530

    Added changes

--- src/components/MyTransactions.vue
+++ src/components/MyTransactions.vue
@@ -1,82 +1,87 @@
 <template>
-    <loading-screen v-if="loadingScreen"></loading-screen>
-    <v-card v-else class="card-design mx-5 mt-5 rounded-lg" outlined>
-      <v-card-title>
-        My Transactions
-        <v-spacer></v-spacer>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search Transactions"
-          single-line
-          hide-details
-          outlined
-          dense
-          clearable
-        ></v-text-field>
-      </v-card-title>
-      <v-data-table
-        class="table-design"
-        :sort-by="sortBy"
-        :headers="headers"
-        :items="transactions"
-        :search="search"
-        show-select
-        mobile-breakpoint="0"
-        no-data-text="No transactions yet"
-      >
-        <template v-slot:[`item.transactionDate`]="{ item }">
-          {{ formatLocalDateTime(item.transactionDate) }}
-        </template>
-      </v-data-table>
-    </v-card>
-  </template>
+  <loading-screen v-if="loadingScreen"></loading-screen>
+  <v-card v-else class="card-design mx-5 mt-5 rounded-lg" outlined>
+    <v-card-title>
+      My Transactions
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search Transactions"
+        single-line
+        hide-details
+        outlined
+        dense
+        clearable
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      class="table-design"
+      :sort-by="sortBy"
+      :headers="headers"
+      :items="transactions"
+      :search="search"
+      show-select
+      mobile-breakpoint="0"
+      no-data-text="No transactions yet"
+    >
+      <template v-slot:[`item.transactionDate`]="{ item }">
+        {{ formatLocalDateTime(item.transactionDate) }}
+      </template>
+      <template v-slot:[`item.amount`]="{ item }">
+        <span>&#8377;</span> {{ item.amount / 100 }}
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
   
   <script>
-  import { getMyTransactions } from "../helpers/backend_helper";
-  import LoadingScreen from "./LoadingScreen.vue";
-  import { formatLocalDateTime } from "./../helpers/common_functions";
+import { getMyTransactions } from "../helpers/backend_helper";
+import LoadingScreen from "./LoadingScreen.vue";
+import { formatLocalDateTime } from "./../helpers/common_functions";
 
-  
-  export default {
-    components: { LoadingScreen },
-    name: "MyTransactions",
-    data() {
-      return {
-        sortBy: [{ key: "id", order: "asc" }],
-        transactions: [],
-        search: "",
-        loadingScreen: false,
-        headers: [
-          {
-            text: "Id",
-            align: "start",
-            value: "id",
-            sortable: false,
-          },
-          { text: "Order Id", value: "orderId", sortable: false },
-          { text: "RazorpayPaymentId", value: "razorpayPaymentId", sortable: false },
-          { text: "Amount", value: "amount", sortable: false },
-          { text: "Transaction Date", value: "transactionDate", sortable: false },
-          { text: "Status", value: "status", sortable: false },
-        ],
-      };
-    },
-    async created() {
-      this.loadingScreen = true;
-      const response = await getMyTransactions();
-      this.transactions = response;
-      this.loadingScreen = false;
-    },
-    methods: {
-        formatLocalDateTime
-    },
-  };
-  </script>
+export default {
+  components: { LoadingScreen },
+  name: "MyTransactions",
+  data() {
+    return {
+      sortBy: [{ key: "id", order: "asc" }],
+      transactions: [],
+      search: "",
+      loadingScreen: false,
+      headers: [
+        {
+          text: "Id",
+          align: "start",
+          value: "id",
+          sortable: false,
+        },
+        { text: "Order Id", value: "orderId", sortable: false },
+        {
+          text: "RazorpayPaymentId",
+          value: "razorpayPaymentId",
+          sortable: false,
+        },
+        { text: "Amount", value: "amount", sortable: false },
+        { text: "Transaction Date", value: "transactionDate", sortable: false },
+        { text: "Status", value: "status", sortable: false },
+      ],
+    };
+  },
+  async created() {
+    this.loadingScreen = true;
+    const response = await getMyTransactions();
+    this.transactions = response;
+    this.loadingScreen = false;
+  },
+  methods: {
+    formatLocalDateTime,
+  },
+};
+</script>
   
   <style lang="scss" scoped>
-  .card-design {
-    padding-bottom: 20px;
-  }
-  
-  </style>
+.card-design {
+  padding-bottom: 20px;
+}
+</style>
